15248ba6d7337364b0e446af3d191fb38a8b90df
gruv19 <grv1992@yandex.ru>
2021-10-06 23:51:56 +0500

    Start config project

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(js)$/, use: "babel-loader", exclude: "/node_modules/" },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: "./src/common.blocks/global/_vars.scss",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.pug",
+      chunks: ["index"],
+    }),
+  ],
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+};
