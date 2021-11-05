88b0899717f45d9a3e06ceae168e537db82ab50b
gruv19 <grv1992@yandex.ru>
2021-11-05 20:02:39 +0500

    Added review block

---
+++ src/common.blocks/review/review.js
@@ -0,0 +1,7 @@
+import "../form/__like-button/__like-button.js";
+
+import "./review.scss";
+
+import img from "/src/images/users/guest1.jpg";
+
+document.querySelector(".review__author-photo").src = img;
--- src/form-elements.js
+++ src/form-elements.js
@@ -6,3 +6,4 @@ import "./common.blocks/header/header.js";
 import "./common.blocks/form/form.js";
 import "./common.blocks/bullet-list/bullet-list.js";
 import "./common.blocks/info-card/info-card.js";
+import "./common.blocks/review/review.js";
