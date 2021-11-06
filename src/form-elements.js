dfda50706414977d62d721254827503bfbb3b06c
gruv19 <grv1992@yandex.ru>
2021-11-06 11:42:50 +0500

    Change with new toggle

--- src/form-elements.js
+++ src/form-elements.js
@@ -11,6 +11,7 @@ import "./common.blocks/review/review.js";
 import "./common.blocks/button/button.js";
 import "./common.blocks/checkbox-button/checkbox-button.js";
 import checkboxList from "./common.blocks/expandable-checkbox-list/expandable-checkbox-list.js";
+import "./common.blocks/toggle/toggle.js";
 
 $(window).on("load", () => {
   checkboxList();
