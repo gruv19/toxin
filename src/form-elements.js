485f5890c41330b94cc2a23f052715fda2450477
gruv19 <grv1992@yandex.ru>
2021-11-06 11:31:43 +0500

    Change with new checkbox-button and expandable-checkbox-list

--- src/form-elements.js
+++ src/form-elements.js
@@ -7,4 +7,11 @@ import "./common.blocks/form/form.js";
 import "./common.blocks/bullet-list/bullet-list.js";
 import "./common.blocks/info-card/info-card.js";
 import "./common.blocks/review/review.js";
+
 import "./common.blocks/button/button.js";
+import "./common.blocks/checkbox-button/checkbox-button.js";
+import checkboxList from "./common.blocks/expandable-checkbox-list/expandable-checkbox-list.js";
+
+$(window).on("load", () => {
+  checkboxList();
+});
