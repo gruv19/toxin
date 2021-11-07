4b3788ddb630cd5829ff2dcfdddf87ee3c281574
gruv19 <grv1992@yandex.ru>
2021-11-07 20:43:46 +0500

    Removed comments, formatted code

--- src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
+++ src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
@@ -1,19 +1,43 @@
 import customDatepicker from "../custom-datepicker/custom-datepicker";
 import "./filter-date-dropdown.scss";
 
-function filterDateDropdown(filterDatepickerSelector) {
-  const filterDatepicker = document.querySelector(filterDatepickerSelector);
-  const filterOutputField = filterDatepicker.querySelector(".filter-date-dropdown__field");
-  const filterInputElement = filterDatepicker.querySelector(".filter-date-dropdown__input");
+function filterDateDropdown(filterDateDropdownSelector) {
+  const filterDatepicker = document.querySelector(filterDateDropdownSelector);
+  const filterOutputField = filterDatepicker.querySelector(
+    ".filter-date-dropdown__field"
+  );
+  const filterInputElement = filterDatepicker.querySelector(
+    ".filter-date-dropdown__input"
+  );
   const filterDatepickerOptions = {
     classes: "filter-date-dropdown__datepicker",
     container: "filter-date-dropdown__group",
     dateFormat: "dd MMM",
     locale: {
-      monthsShort: ["янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"],
+      monthsShort: [
+        "янв",
+        "фев",
+        "мар",
+        "апр",
+        "май",
+        "июн",
+        "июл",
+        "авг",
+        "сен",
+        "окт",
+        "ноя",
+        "дек",
+      ],
     },
-    onSelect({ date, formattedDate, datepicker }) {
-      filterOutputField.textContent = date[0] ? filterInputElement.value : "Выберите даты";
+    onSelect({ date: date, datepicker: datepicker }) {
+      const rangeFrom = datepicker.$content.querySelector(".-range-from-");
+      const rangeTo = datepicker.$content.querySelector(".-range-to-");
+      if (!rangeTo && rangeFrom) {
+        rangeFrom.classList.add("-range-to-");
+      }
+      filterOutputField.textContent = date[0]
+        ? filterInputElement.value
+        : "Выберите даты";
     },
     onShow(isFinished) {
       filterOutputField.classList.add("filter-date-dropdown__field--active");
@@ -28,8 +52,10 @@ function filterDateDropdown(filterDatepickerSelector) {
       }
     },
   };
-  customDatepicker(filterDatepickerSelector + " .filter-date-dropdown__input", filterDatepickerOptions);
+  customDatepicker(
+    filterDateDropdownSelector + " .filter-date-dropdown__input",
+    filterDatepickerOptions
+  );
 }
 
 export default filterDateDropdown;
-
