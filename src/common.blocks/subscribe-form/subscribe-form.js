f1e32c8983da9ff273e0154667320f6a65b0d9b1
gruv19 <grv1992@yandex.ru>
2021-11-09 23:01:23 +0500

    Subscribe-form moved to a self-block

--- src/common.blocks/subscribe-form/subscribe-form.js
+++ src/common.blocks/subscribe-form/subscribe-form.js
@@ -1,1 +1,23 @@
 import "./subscribe-form.scss";
+
+function subscribe() {
+  async function sendRequest(url, body) {
+    let response = await fetch(url, {
+      method: "POST",
+      body: body,
+    });
+    return await response.json();
+  }
+
+  const forms = document.querySelectorAll(".subscribe-form");
+  forms.forEach((form) => {
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const data = new FormData(e.target);
+      alert("Теперь на ваш адрес " + data.get("email") + " будут приходить наши новости.");
+      // sendRequest(e.target.action, data);
+    });
+  });
+}
+
+export default subscribe;
