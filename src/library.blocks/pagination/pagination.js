852e645ee81d34aa25829d4f2c17ea877487ee4c
gruv19 <grv1992@yandex.ru>
2021-12-11 11:47:40 +0500

    Pixel perfect for Pagination block

--- src/library.blocks/pagination/pagination.js
+++ src/library.blocks/pagination/pagination.js
@@ -1,21 +1,35 @@
-import "paginationjs/dist/pagination.css"
-import "paginationjs/dist/pagination.js"
+import "paginationjs/dist/pagination.css";
+import "paginationjs/dist/pagination.js";
 
 import "./pagination.scss";
 
-function pagination(paginateSelector, dataSelector, dataSource, templateFunction) {
+function pagination(
+  paginateSelector,
+  dataSelector,
+  dataSource,
+  templateFunction
+) {
   $(paginateSelector).pagination({
     dataSource: dataSource,
     pageSize: 12,
+    pageRange: 1,
     autoHidePrevious: true,
     autoHideNext: true,
     className: "my-pagination",
     prevText: "",
     nextText: "",
-    callback: function(data) {
-        const html = templateFunction(data);
-        $(dataSelector).html(html);
-    }
+    showNavigator: true,
+    formatNavigator: function(currentPage, totalPage, totalNumber) {
+      let resTotal = totalNumber;
+      if (totalNumber > 100) {
+        resTotal = "100+";
+      }
+      return `<span>${currentPage} - ${totalPage} из ${resTotal} вариантов аренды</span>`
+    },
+    callback: function (data) {
+      const html = templateFunction(data);
+      $(dataSelector).html(html);
+    },
   });
 }
 
