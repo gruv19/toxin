aacae8dba86404317c40d50de8d71b41cfe4de00
gruv19 <grv1992@yandex.ru>
2021-12-24 11:52:58 +0500

    Added base styles and search-card block

---
+++ src/cards.js
@@ -0,0 +1,6 @@
+import "./common.blocks/page/page.js";
+import "./common.blocks/main/main.scss";
+
+import "./common.blocks/uikit-header/uikit-header.js";
+import "./common.blocks/search-card/search-card.js";
+import "./templates/cards/cards.scss";
