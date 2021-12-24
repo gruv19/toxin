ea9bfe62228b76c74b58a3003d3f5edb511493b4
gruv19 <grv1992@yandex.ru>
2021-12-24 14:03:01 +0500

    Add login-card to page 'Cards'

--- src/cards.js
+++ src/cards.js
@@ -4,9 +4,11 @@ import "./common.blocks/main/main.scss";
 import "./common.blocks/uikit-header/uikit-header.js";
 import searchCard from "./common.blocks/search-card/search-card.js";
 import registrationCard from "./common.blocks/registration-card/registration-card.js";
+import loginCard from "./common.blocks/login-card/login-card.js";
 import "./templates/cards/cards.scss";
 
 $(window).on("load", () => {
   searchCard();
   registrationCard();
+  loginCard();
 });
