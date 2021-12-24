4e1b2b94808dfdd7734be950a3c0af7d649bcdc0
gruv19 <grv1992@yandex.ru>
2021-12-24 13:24:10 +0500

    Add registration-card to page 'Cards'

--- src/cards.js
+++ src/cards.js
@@ -2,5 +2,11 @@ import "./common.blocks/page/page.js";
 import "./common.blocks/main/main.scss";
 
 import "./common.blocks/uikit-header/uikit-header.js";
-import "./common.blocks/search-card/search-card.js";
+import searchCard from "./common.blocks/search-card/search-card.js";
+import registrationCard from "./common.blocks/registration-card/registration-card.js";
 import "./templates/cards/cards.scss";
+
+$(window).on("load", () => {
+  searchCard();
+  registrationCard();
+});
