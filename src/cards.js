5bff6788465939eefa4948d5fe1b0fff0888d1a5
gruv19 <grv1992@yandex.ru>
2021-12-27 17:30:35 +0500

    Add booking-card to page 'Cards'

--- src/cards.js
+++ src/cards.js
@@ -5,10 +5,12 @@ import "./common.blocks/uikit-header/uikit-header.js";
 import searchCard from "./common.blocks/search-card/search-card.js";
 import registrationCard from "./common.blocks/registration-card/registration-card.js";
 import loginCard from "./common.blocks/login-card/login-card.js";
+import bookingCard from "./common.blocks/booking-card/booking-card.js";
 import "./templates/cards/cards.scss";
 
 $(window).on("load", () => {
   searchCard();
   registrationCard();
   loginCard();
+  bookingCard({ number: 888, category: "ЛЮКС", cost: 9990, service: 0, additional: 300 }, 2179);
 });
