4d3bde772fe8224d4577e144471264dd4d27d82c
gruv19 <grv1992@yandex.ru>
2021-12-30 10:20:23 +0500

    Create room-card block

---
+++ src/common.blocks/room-card/room-card.js
@@ -0,0 +1,9 @@
+import rateButton from "../../library.blocks/rate-button/rate-button.js";
+import "./room-card.scss";
+
+function roomCard() {
+  rateButton(true);
+  console.log("roomCard work!");
+}
+
+export default roomCard;
