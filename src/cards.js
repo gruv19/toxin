1bb8b810166bdf7a568b514cdfafea708a1a5bd2
gruv19 <grv1992@yandex.ru>
2022-01-03 10:51:23 +0500

    Add air-datepicker to page 'Cards'

--- src/cards.js
+++ src/cards.js
@@ -7,12 +7,25 @@ import registrationCard from "./common.blocks/registration-card/registration-car
 import loginCard from "./common.blocks/login-card/login-card.js";
 import bookingCard from "./common.blocks/booking-card/booking-card.js";
 import roomCard from "./common.blocks/room-card/room-card.js";
+import customDatepicker from "./library.blocks/custom-datepicker/custom-datepicker.js";
 import "./templates/cards/cards.scss";
 
 $(window).on("load", () => {
   searchCard();
   registrationCard();
   loginCard();
-  bookingCard({ number: 888, category: "ЛЮКС", cost: 9990, service: 0, additional: 300 }, 2179);
+  bookingCard(
+    { number: 888, category: "ЛЮКС", cost: 9990, service: 0, additional: 300 },
+    2179
+  );
   roomCard();
+  customDatepicker(".cards__input", {
+    inline: true,
+    buttons: [
+      "clear",
+      {
+        content: "Применить",
+      },
+    ],
+  });
 });
