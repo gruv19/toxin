df3bf2d8822087bc87c79e6f419561d1d4ec9278
gruv19 <grv1992@yandex.ru>
2022-01-11 21:25:29 +0500

    Changed nav block with smartmenus plugin

--- src/common.blocks/nav/nav.js
+++ src/common.blocks/nav/nav.js
@@ -1,1 +1,19 @@
+import "../account/account.js";
+import "../../library.blocks/main-menu-state/main-menu-state.js";
+import customMenu from "../../library.blocks/custom-menu/custom-menu.js";
+
 import "./nav.scss";
+
+function nav(navSelector) {
+  const nav = document.querySelector(navSelector);
+  const submenuLinks = nav.querySelectorAll(".nav__link--submenu");
+  customMenu(".nav__list");
+  submenuLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("here");
+    });
+  });
+}
+
+export default nav;
