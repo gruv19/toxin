dbd03aef0999f9c0ae62356e0af1b44608bb29ba
gruv19 <grv1992@yandex.ru>
2022-01-11 21:07:20 +0500

    Added smartmenus and created custom-menu

---
+++ src/library.blocks/custom-menu/custom-menu.js
@@ -0,0 +1,14 @@
+import "./smartmenus/jquery.smartmenus.min.js";
+import "./smartmenus/sm-core-css.css";
+
+function customMenu(menuSelector) {
+  $(menuSelector).smartmenus({
+    subIndicatorsPos: "append",
+    subIndicatorsText: "",
+    subMenusSubOffsetY: 0,
+    mainMenuSubOffsetY: 25,
+    mainMenuSubOffsetX: -10,
+  });
+}
+
+export default customMenu;
