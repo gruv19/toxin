f4e7a996effbd1f203d372e2e8afd89273f23a46
gruv19 <grv1992@yandex.ru>
2022-01-13 22:49:12 +0500

    Modified footer with new blocks

--- src/common.blocks/footer/footer.js
+++ src/common.blocks/footer/footer.js
@@ -1,4 +1,14 @@
+// import "../widgets/widgets.js";
+// import "../copyright/copyright.js";
+import "../widget/widget.js";
+import subscribe from "../../library.blocks/subscribe-form/subscribe-form.js";
+import "../socials/socials.js"
+
 import "./footer.scss";
 
-import "../widgets/widgets.js";
-import "../copyright/copyright.js";
+function footer() {
+  subscribe();
+  console.log("I work");
+}
+
+export default footer;
