ae51cf34343b16f384f3f480d2bff8171cf946b7
gruv19 <grv1992@yandex.ru>
2022-01-13 22:53:24 +0500

    Footer added into headers-and-footers page

--- src/common.blocks/footer/footer.js
+++ src/common.blocks/footer/footer.js
@@ -1,5 +1,3 @@
-// import "../widgets/widgets.js";
-// import "../copyright/copyright.js";
 import "../widget/widget.js";
 import subscribe from "../../library.blocks/subscribe-form/subscribe-form.js";
 import "../socials/socials.js"
@@ -8,7 +6,6 @@ import "./footer.scss";
 
 function footer() {
   subscribe();
-  console.log("I work");
 }
 
 export default footer;
--- src/headers-and-footers.js
+++ src/headers-and-footers.js
@@ -4,10 +4,11 @@ import "./common.blocks/page/page.js";
 import "./common.blocks/uikit-header/uikit-header.js";
 
 import header from "./common.blocks/header/header.js";
-import "./common.blocks/footer/footer.js";
+import footer from "./common.blocks/footer/footer.js";
 
 import "./templates/headers-and-footers/headers-and-footers.scss";
 
 $(window).on("load", () => {
   header(".header__menu");
+  footer();
 });
