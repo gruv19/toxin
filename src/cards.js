b819a006d9ea869cb6dec7614484a3a27c0325e9
gruv19 <grv1992@yandex.ru>
2022-01-23 14:55:13 +0500

    refactor: modified with eslint rules

--- src/cards.js
+++ src/cards.js
@@ -1,30 +1,32 @@
-import "normalize.css";
-import "./common.blocks/page/page.js";
+import 'normalize.css';
+import './common.blocks/page/page';
 
-import "./common.blocks/uikit-header/uikit-header.js";
-import searchCard from "./common.blocks/search-card/search-card.js";
-import registrationCard from "./common.blocks/registration-card/registration-card.js";
-import loginCard from "./common.blocks/login-card/login-card.js";
-import bookingCard from "./common.blocks/booking-card/booking-card.js";
-import roomCard from "./common.blocks/room-card/room-card.js";
-import customDatepicker from "./library.blocks/custom-datepicker/custom-datepicker.js";
-import "./templates/cards/cards.scss";
+import './common.blocks/uikit-header/uikit-header';
+import searchCard from './common.blocks/search-card/search-card';
+import registrationCard from './common.blocks/registration-card/registration-card';
+import loginCard from './common.blocks/login-card/login-card';
+import bookingCard from './common.blocks/booking-card/booking-card';
+import roomCard from './common.blocks/room-card/room-card';
+import customDatepicker from './library.blocks/custom-datepicker/custom-datepicker';
+import './templates/cards/cards.scss';
 
-$(window).on("load", () => {
+$(window).on('load', () => {
   searchCard();
   registrationCard();
   loginCard();
   bookingCard(
-    { number: 888, category: "ЛЮКС", cost: 9990, service: 0, additional: 300 },
-    2179
+    {
+      number: 888, category: 'ЛЮКС', cost: 9990, service: 0, additional: 300,
+    },
+    2179,
   );
   roomCard();
-  customDatepicker(".cards__input", {
+  customDatepicker('.cards__input', {
     inline: true,
     buttons: [
-      "clear",
+      'clear',
       {
-        content: "Применить",
+        content: 'Применить',
       },
     ],
   });
