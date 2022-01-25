a759e4e8d19d47f148d928390be0f9da65a1ab20
gruv19 <grv1992@yandex.ru>
2022-01-25 10:40:31 +0500

    fix: added blocks 'button' and 'textfield'

--- src/common.blocks/login-card/login-card.js
+++ src/common.blocks/login-card/login-card.js
@@ -1,20 +1,16 @@
-import "./login-card.scss";
+import '../../library.blocks/textfield/textfield';
+import '../../library.blocks/button/button';
+import './login-card.scss';
 
 function loginCard() {
-  const loginForm = document.querySelector(".login-card__form");
-  const registrarionBtn = document.querySelector(".login-card__button-registration");
+  const loginForm = document.querySelector('.login-card__form');
 
-  loginForm.addEventListener("submit", (e) => {
+  loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    for (let key of data.keys()) {
-      console.log(`${key}: ${data.get(key)}`);
-    }
-  });
-
-  registrarionBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("Тут должно быть модальное окно для регистрации!:)");
+    // const data = new FormData(e.target);
+    // for (let key of data.keys()) {
+    //   console.log(`${key}: ${data.get(key)}`);
+    // }
   });
 }
 
