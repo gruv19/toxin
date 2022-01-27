c91027c92709b3305914d8cac654229e5be18fe8
gruv19 <grv1992@yandex.ru>
2022-01-27 16:33:23 +0500

    refactor: changed 'date-dropdown' according to requirements

--- src/library.blocks/date-dropdown/date-dropdown.js
+++ src/library.blocks/date-dropdown/date-dropdown.js
@@ -1,77 +1,72 @@
-import customDatepicker from "../custom-datepicker/custom-datepicker";
-import "../label/label.js";
-import "./date-dropdown.scss";
+import customDatepicker from '../custom-datepicker/custom-datepicker';
+import '../label/label';
+import './date-dropdown.scss';
 
 function dateDropdown(dateDropdownSelector, selectedDates = false) {
-  const dateDropdown = document.querySelector(dateDropdownSelector);
-  const outputFieldFrom = dateDropdown.querySelector(
-    ".date-dropdown__field--from"
-  );
-  const outputFieldto = dateDropdown.querySelector(".date-dropdown__field--to");
+  const dateDropdownElement = document.querySelector(dateDropdownSelector);
+  const outputFieldFrom = dateDropdownElement.querySelector('.date-dropdown__field--from');
+  const outputFieldto = dateDropdownElement.querySelector('.date-dropdown__field--to');
 
   const applyButton = {
-    content: "Применить",
+    content: 'Применить',
     onClick: (dp) => {
       dp.hide();
       dp.$datepicker.dispatchEvent(
-        new CustomEvent("changeDate", {
+        new CustomEvent('changeDate', {
           bubbles: true,
           cancelable: true,
           detail: {
             dateFrom: dp.selectedDates[0],
             dateTo: dp.selectedDates[1],
           },
-        })
+        }),
       );
     },
   };
 
   const datepickerOptions = {
-    buttons: ["clear", applyButton],
-    classes: "date-dropdown__datepicker",
-    container: "date-dropdown__group",
-    dateFormat: "dd.MM.yyyy",
+    buttons: ['clear', applyButton],
+    classes: 'date-dropdown__datepicker',
+    container: 'date-dropdown__group',
+    dateFormat: 'dd.MM.yyyy',
     onSelect({ date, formattedDate, datepicker }) {
-      const rangeFrom = datepicker.$content.querySelector(".-range-from-");
-      const rangeTo = datepicker.$content.querySelector(".-range-to-");
+      const rangeFrom = datepicker.$content.querySelector('.-range-from-');
+      const rangeTo = datepicker.$content.querySelector('.-range-to-');
       if (!rangeTo && rangeFrom) {
-        rangeFrom.classList.add("-range-to-");
+        rangeFrom.classList.add('-range-to-');
       }
-      outputFieldFrom.textContent = date[0] ? formattedDate[0] : "ДД.ММ.ГГГГ";
-      outputFieldto.textContent = date[1] ? formattedDate[1] : "ДД.ММ.ГГГГ";
+      outputFieldFrom.textContent = date[0] ? formattedDate[0] : 'ДД.ММ.ГГГГ';
+      outputFieldto.textContent = date[1] ? formattedDate[1] : 'ДД.ММ.ГГГГ';
       if (formattedDate.length === 2) {
         datepicker.$datepicker.dispatchEvent(
-          new CustomEvent("changeDate", {
+          new CustomEvent('changeDate', {
             bubbles: true,
             cancelable: true,
             detail: {
               dateFrom: datepicker.selectedDates[0],
               dateTo: datepicker.selectedDates[1],
             },
-          })
+          }),
         );
       }
     },
     onShow(isFinished) {
-      outputFieldFrom.classList.add("date-dropdown__field--active");
-      outputFieldto.classList.add("date-dropdown__field--active");
+      outputFieldFrom.classList.add('date-dropdown__field--active');
+      outputFieldto.classList.add('date-dropdown__field--active');
       if (isFinished) {
-        return;
+        return; // eslint-disable-line
       }
     },
     onHide(isFinished) {
-      outputFieldFrom.classList.remove("date-dropdown__field--active");
-      outputFieldto.classList.remove("date-dropdown__field--active");
+      outputFieldFrom.classList.remove('date-dropdown__field--active');
+      outputFieldto.classList.remove('date-dropdown__field--active');
       if (isFinished) {
-        return;
+        return; // eslint-disable-line
       }
     },
   };
 
-  const datepicker = customDatepicker(
-    dateDropdownSelector + " .date-dropdown__input",
-    datepickerOptions
-  );
+  const datepicker = customDatepicker(`${dateDropdownSelector} .date-dropdown__input`, datepickerOptions);
 
   if (selectedDates) {
     datepicker.selectDate(selectedDates);
