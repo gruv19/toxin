3bd056ad2f7b10cabf6918a73b9ad0466bff6025
gruv19 <grv1992@yandex.ru>
2022-01-27 16:32:51 +0500

    feat: added parameters verification

--- src/library.blocks/custom-menu/custom-menu.js
+++ src/library.blocks/custom-menu/custom-menu.js
@@ -1,14 +1,18 @@
 import './smartmenus/jquery.smartmenus.min';
 import './smartmenus/sm-core-css.css';
 
-function customMenu(menuSelector) {
-  $(menuSelector).smartmenus({
-    subIndicatorsPos: 'append',
-    subIndicatorsText: '',
-    subMenusSubOffsetY: 0,
-    mainMenuSubOffsetY: 25,
-    mainMenuSubOffsetX: -10,
-  });
+function customMenu(menuSelector = '') {
+  if (menuSelector) {
+    $(menuSelector).smartmenus({
+      subIndicatorsPos: 'append',
+      subIndicatorsText: '',
+      subMenusSubOffsetY: 0,
+      mainMenuSubOffsetY: 25,
+      mainMenuSubOffsetX: -10,
+    });
+  } else {
+    throw Error('Не указан селектор меню');
+  }
 }
 
 export default customMenu;
