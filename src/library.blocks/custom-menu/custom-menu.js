d931d0782494824231ffdb18c85dfe02356ef124
gruv19 <grv1992@yandex.ru>
2022-01-27 14:44:27 +0500

    refactor: changed 'custom-menu' according to requirements

--- src/library.blocks/custom-menu/custom-menu.js
+++ src/library.blocks/custom-menu/custom-menu.js
@@ -1,10 +1,10 @@
-import "./smartmenus/jquery.smartmenus.min.js";
-import "./smartmenus/sm-core-css.css";
+import './smartmenus/jquery.smartmenus.min';
+import './smartmenus/sm-core-css.css';
 
 function customMenu(menuSelector) {
   $(menuSelector).smartmenus({
-    subIndicatorsPos: "append",
-    subIndicatorsText: "",
+    subIndicatorsPos: 'append',
+    subIndicatorsText: '',
     subMenusSubOffsetY: 0,
     mainMenuSubOffsetY: 25,
     mainMenuSubOffsetX: -10,
