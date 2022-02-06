70fb2308a9926755152603cd219e4489fcca1f94
gruv19 <grv1992@yandex.ru>
2022-02-06 16:03:14 +0500

    refactor: added $ to name variabled with jquery API

--- src/library.blocks/masked-textfield/masked-textfield.js
+++ src/library.blocks/masked-textfield/masked-textfield.js
@@ -1,7 +1,7 @@
 import './masked-textfield.scss';
 
 function maskedTextfield() {
-  const maskedTextField = $('.js-masked-textfield__input');
+  const $maskedTextField = $('.js-masked-textfield__input');
   const regArray = [
     {
       regexp: /^([0-3])$/,
@@ -45,37 +45,37 @@ function maskedTextfield() {
     },
   ];
 
-  maskedTextField.on('input', (e) => {
-    let inputedValue = $(e.target).val();
-    if (regArray[0].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[0].regexp, regArray[0].replacement));
-    } else if (regArray[1].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[1].regexp, regArray[1].replacement));
-    } else if (regArray[2].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[2].regexp, regArray[2].replacement));
-    } else if (regArray[3].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[3].regexp, regArray[3].replacement));
-    } else if (regArray[4].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[4].regexp, regArray[4].replacement));
-    } else if (regArray[5].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[5].regexp, regArray[5].replacement));
-    } else if (regArray[6].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[6].regexp, regArray[6].replacement));
-    } else if (regArray[7].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[7].regexp, regArray[7].replacement));
-    } else if (regArray[8].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[8].regexp, regArray[8].replacement));
-    } else if (regArray[9].regexp.test(inputedValue)) {
-      $(e.target).val(inputedValue.replace(regArray[9].regexp, regArray[9].replacement));
+  $maskedTextField.on('input', (e) => {
+    let $inputedValue = $(e.target).val();
+    if (regArray[0].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[0].regexp, regArray[0].replacement));
+    } else if (regArray[1].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[1].regexp, regArray[1].replacement));
+    } else if (regArray[2].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[2].regexp, regArray[2].replacement));
+    } else if (regArray[3].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[3].regexp, regArray[3].replacement));
+    } else if (regArray[4].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[4].regexp, regArray[4].replacement));
+    } else if (regArray[5].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[5].regexp, regArray[5].replacement));
+    } else if (regArray[6].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[6].regexp, regArray[6].replacement));
+    } else if (regArray[7].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[7].regexp, regArray[7].replacement));
+    } else if (regArray[8].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[8].regexp, regArray[8].replacement));
+    } else if (regArray[9].regexp.test($inputedValue)) {
+      $(e.target).val($inputedValue.replace(regArray[9].regexp, regArray[9].replacement));
     } else {
-      inputedValue = inputedValue.replace(/\D+/g, '');
-      inputedValue = inputedValue.slice(0, 8);
-      while (inputedValue.length) {
+      $inputedValue = $inputedValue.replace(/\D+/g, '');
+      $inputedValue = $inputedValue.slice(0, 8);
+      while ($inputedValue.length) {
         let flag = false;
         /* eslint-disable */
         regArray.forEach(reg => {
-          if (reg.regexp.test(inputedValue)) {
-            inputedValue = inputedValue.replace(reg.regexp, reg.replacement);
+          if (reg.regexp.test($inputedValue)) {
+            $inputedValue = $inputedValue.replace(reg.regexp, reg.replacement);
             flag = true;
           }
         });
@@ -83,9 +83,9 @@ function maskedTextfield() {
         if (flag) {
           break;
         }
-        inputedValue = inputedValue.slice(0, inputedValue.length - 1);
+        $inputedValue = $inputedValue.slice(0, $inputedValue.length - 1);
       }
-      $(e.target).val(inputedValue);
+      $(e.target).val($inputedValue);
     }
   });
 }
