279ad751130f3898e1102974d579a9ecc325ae10
gruv19 <grv1992@yandex.ru>
2022-02-06 15:54:28 +0500

    refactor: changed 'overlay-glide' according to requirements

--- src/library.blocks/overlay-glide/overlay-glide.js
+++ src/library.blocks/overlay-glide/overlay-glide.js
@@ -1,11 +1,11 @@
-import "../../../node_modules/@glidejs/glide/src/assets/sass/glide.core.scss";
-import Glide from "@glidejs/glide";
+import '@glidejs/glide/src/assets/sass/glide.core.scss';
+import Glide from '@glidejs/glide';
 
-import  "./overlay-glide.scss";
+import './overlay-glide.scss';
 
-function overlayGlide(glideSelector = ".overlay-glide") {
-  new Glide(glideSelector, {
-    type: "carousel",
+function overlayGlide() {
+  new Glide('.js-overlay-glide', {
+    type: 'carousel',
     startAt: 0,
     perView: 1,
     autoplay: 3000,
