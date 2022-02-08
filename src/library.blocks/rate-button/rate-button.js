956fc6a8bb6280e6b25dc3e0e143b93bb1348d69
gruv19 <grv1992@yandex.ru>
2022-02-08 22:29:49 +0500

    refactor: changed 'rate-button' according to requirements

--- src/library.blocks/rate-button/rate-button.js
+++ src/library.blocks/rate-button/rate-button.js
@@ -1,18 +1,19 @@
-import "star-rating.js/dist/star-rating.css";
-import StarRating from "star-rating.js";
+import 'star-rating.js/dist/star-rating.css';
+import StarRating from 'star-rating.js';
 
-import "./rate-button.scss";
+import './rate-button.scss';
 
-function rateButton(prebuilt = false) {
-  return new StarRating(".rate-button", {
+function rateButton(isForFeedback = false) {
+  const rateElementSelector = isForFeedback ? '.js-rate-button__select' : '.js-rate-button';
+  return new StarRating(rateElementSelector, {
     tooltip: false,
     classNames: {
-      active: "my-gl-active",
-      base: "my-gl-star-rating",
-      selected: "my-gl-selected",
+      active: 'rate-button__star--active',
+      base: 'rate-button__star-rating',
+      selected: 'rate-button__star--selected',
     },
     maxStars: 5,
-    prebuilt: prebuilt,
+    prebuilt: !isForFeedback,
   });
 }
 
