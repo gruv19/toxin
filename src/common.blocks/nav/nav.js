6c142ba126039520c7d613b53ced689e876c8377
gruv19 <grv1992@yandex.ru>
2022-02-10 23:01:30 +0500

    refactor: changed 'nav' according to requirements

--- src/common.blocks/nav/nav.js
+++ src/common.blocks/nav/nav.js
@@ -1,10 +1,11 @@
 import '../account/account';
+
 import '../../library.blocks/main-menu-state/main-menu-state';
 import customMenu from '../../library.blocks/custom-menu/custom-menu';
 
 import './nav.scss';
 
-function nav(navSelector) {
+function nav(navSelector = '.js-nav') {
   const navEl = document.querySelector(navSelector);
   const submenuLinks = navEl.querySelectorAll('.nav__link--submenu');
   customMenu('.nav__list');
