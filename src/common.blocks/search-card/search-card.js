07632c7163ab7a134eb8c8a197c0093d6bae7f31
gruv19 <grv1992@yandex.ru>
2022-02-15 22:58:53 +0500

    refactor: changed 'search-card' according to requirements

--- src/common.blocks/search-card/search-card.js
+++ src/common.blocks/search-card/search-card.js
@@ -1,27 +1,21 @@
-import dateDropdown from "../../library.blocks/date-dropdown/date-dropdown.js";
-import dropdown from "../../library.blocks/dropdown/dropdown.js";
-import "../../library.blocks/label/label.js";
-import "../../library.blocks/button/button.js";
+import '../../library.blocks/button/button';
+import dateDropdown from '../../library.blocks/date-dropdown/date-dropdown';
+import dropdown from '../../library.blocks/dropdown/dropdown';
+import '../../library.blocks/label/label';
 
-import "./search-card.scss";
+import './search-card.scss';
 
-function searchCard() {
-  dropdown(".search-card__dropdown", [
-    { one: "гость", twoToFour: "гостя", moreThanFive: "гостей" },
+function searchCard(searchFormSelector = '.js-search-card__form') {
+  dropdown(`${searchFormSelector} .js-dropdown`, [
+    { one: 'гость', twoToFour: 'гостя', moreThanFive: 'гостей' },
   ]);
 
-  const datepicker = dateDropdown(".search-card__date-dropdown");
-  const searchForm = document.querySelector(".search-card__form");
+  dateDropdown(`${searchFormSelector} .js-date-dropdown`);
+  const searchForm = document.querySelector(searchFormSelector);
 
-  searchForm.addEventListener("submit", (e) => {
+  searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    data.append("date-from", datepicker.selectedDates[0]);
-    data.append("date-to", datepicker.selectedDates[1]);
-    const url = new URL("/search-room.html", window.location.origin);
-    for (let key of data.keys()) {
-      url.searchParams.set(key, data.get(key))
-    }
+    const url = new URL('/search-room.html', window.location.origin);
     window.location.href = url.href;
   });
 }
