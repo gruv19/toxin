dcdfd909f9842b14cf9d9781da4f91c915e4c889
gruv19 <grv1992@yandex.ru>
2022-02-15 21:34:52 +0500

    refactor: changed 'login-card' according to requirements

--- src/common.blocks/login-card/login-card.js
+++ src/common.blocks/login-card/login-card.js
@@ -1,16 +1,13 @@
-import '../../library.blocks/textfield/textfield';
 import '../../library.blocks/button/button';
+import '../../library.blocks/textfield/textfield';
+
 import './login-card.scss';
 
-function loginCard() {
-  const loginForm = document.querySelector('.login-card__form');
+function loginCard(loginFormSelector = '.js-login-card__form') {
+  const loginForm = document.querySelector(loginFormSelector);
 
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // const data = new FormData(e.target);
-    // for (let key of data.keys()) {
-    //   console.log(`${key}: ${data.get(key)}`);
-    // }
   });
 }
 
