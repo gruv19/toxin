3064f97105c5f4db02d6703b60f58bbb8db05947
gruv19 <grv1992@yandex.ru>
2022-02-15 22:14:58 +0500

    refactor: changed 'registration-card' according to requirements

--- src/common.blocks/registration-card/registration-card.js
+++ src/common.blocks/registration-card/registration-card.js
@@ -1,28 +1,19 @@
-import "../../library.blocks/textfield/textfield.js";
-import "../../library.blocks/radio-button/radio-button.js";
-import "../../library.blocks/label/label.js";
-import maskedTextfield from "../../library.blocks/masked-textfield/masked-textfield.js";
-import "../../library.blocks/toggle/toggle.js";
-import "../../library.blocks/button/button.js";
+import '../../library.blocks/button/button';
+import '../../library.blocks/label/label';
+import maskedTextfield from '../../library.blocks/masked-textfield/masked-textfield';
+import '../../library.blocks/radio-button/radio-button';
+import '../../library.blocks/textfield/textfield';
+import '../../library.blocks/toggle/toggle';
 
-import "./registration-card.scss";
+import './registration-card.scss';
 
-function registrationCard() {
+function registrationCard(registrationFormSelector = '.js-registration-card__form') {
   maskedTextfield();
 
-  const registrationForm = document.querySelector(".registration-card__form");
-  const loginBtn = document.querySelector(".registration-card__button-login");
-  registrationForm.addEventListener("submit", (e) => {
+  const registrationForm = document.querySelector(registrationFormSelector);
+  registrationForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    for (let key of data.keys()) {
-      console.log(`${key}: ${data.get(key)}`);
-    }
   });
-  loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("Тут должно быть модальное окно для входа!:)");
-  })
 }
 
 export default registrationCard;
