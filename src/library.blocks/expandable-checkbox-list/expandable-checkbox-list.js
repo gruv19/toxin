1294535e330ce24d1f1c360e8db773a3bf2a2e28
gruv19 <grv1992@yandex.ru>
2022-02-28 20:36:05 +0500

    chore: fixed transitions, added expanded flag

--- src/library.blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/library.blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -6,6 +6,9 @@ import './expandable-checkbox-list.scss';
 function checkboxList() {
   const checkboxListLabels = document.querySelectorAll('.js-expandable-checkbox-list');
   checkboxListLabels.forEach((item) => {
+    if (item.classList.contains('.expandable-checkbox-list--expanded')) {
+      return;
+    }
     item.addEventListener('click', (e) => {
       const target = e.target.closest('.expandable-checkbox-list');
       e.stopPropagation();
