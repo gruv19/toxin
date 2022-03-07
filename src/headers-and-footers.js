f574937aacd2627854abf05e4134c832ff7f0504
gruv19 <grv1992@yandex.ru>
2022-03-07 11:22:10 +0500

    refactor: changed 'headers-and-footers' according to requirements

--- src/headers-and-footers.js
+++ src/headers-and-footers.js
@@ -9,6 +9,6 @@ import footer from './common.blocks/footer/footer';
 import './templates/headers-and-footers/headers-and-footers.scss';
 
 $(window).on('load', () => {
-  header('.header__menu');
+  header();
   footer();
 });
